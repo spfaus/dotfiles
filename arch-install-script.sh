8b651870ca15273d39656b6eb3c5b1a5095f868a
Simon Alexander Pfaus <simon.pfaus@web.de>
2020-11-29 01:11:24 +0100

    Update arch-install-script.sh
    WIP

--- arch-install-script.sh
+++ arch-install-script.sh
@@ -2,6 +2,8 @@
 
 loadkeys de-latin1
 
+pacman -Syu
+
 timedatectl set-ntp true
 
 cfdisk /dev/nvme0n1
@@ -37,7 +39,35 @@ echo "127.0.0.1   localhost" >> /etc/hosts
 echo "::1         localhost" >> /etc/hosts
 echo "127.0.1.1   hp14s.localdomain hp14s" >> /etc/hosts
 
-# root password
+echo "Enter root password"
 passwd
 
-echo "All done!"
+grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+
+ln -s /usr/bin/nvim /usr/bin/vim
+ln -s /usr/bin/nvim /usr/bin/vi
+
+useradd -m simon
+echo "Enter user password"
+passwd simon
+#sed -i -e 's/# Defaults targetpw/Defaults targetpw/g' /etc/sudoers # Not having this set helps with further configuration. This should be set at the end of the script or if an error occurs. 
+sed -i -e 's/# ALL ALL=(ALL) ALL/ALL ALL=(ALL) ALL/g' /etc/sudoers
+
+git clone https://aur.archlinux.org/yay-git.git
+chown -R simon:simon ./yay-git
+cd yay-git
+su -c "makepkg -si" simon
+cd ..
+rm -rf yay-git
+
+su -c "yay -S rtl8821ce-dkms-git" simon
+dkms autoinstall
+#nmtui
+
+systemctl enable gdm.service
+systemctl edit gdm
+
+sed -i -e 's/# Defaults targetpw/Defaults targetpw/g'
+
+reboot
