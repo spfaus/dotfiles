e6d770526e6917cc52a88086c7c72c18ccd8c684
Simon Alexander Pfaus <simon.pfaus@web.de>
2020-11-30 22:13:55 +0100

    Update arch-chroot.sh

--- arch-chroot.sh
+++ arch-chroot.sh
@@ -19,5 +19,5 @@ passwd $MY_USERNAME
 sed -i -e 's/# Defaults targetpw/Defaults targetpw/g' /etc/sudoers
 sed -i -e 's/# ALL ALL=(ALL) ALL/ALL ALL=(ALL) ALL/g' /etc/sudoers
 
-su -c "git clone https://github.com/simon-a-p/dotfiles.git $HOME" $MY_USERNAME
+su -c "git clone https://github.com/simon-a-p/dotfiles.git /home/$MY_USERNAME/dotfiles" $MY_USERNAME
 su - $MY_USERNAME
