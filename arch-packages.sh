8dd4927cd19d5ea803c2aa0ae05878e3a5158850
Simon Alexander Pfaus <simon.pfaus@web.de>
2020-11-30 21:11:11 +0100

    Update arch-packages.sh

--- arch-packages.sh
+++ arch-packages.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+set -ex
 
-pacman -S base base-devel linux linux-firmware neovim reflector sudo man-db man-pages texinfo networkmanager curl git firefox-developer-edition grub efibootmgr amd-ucode dkms linux-headers xorg xorg-server gdm i3-gaps i3blocks i3status i3lock dmenu alacritty
+sudo pacman -Syu
+sudo pacman -S base base-devel linux linux-firmware neovim reflector sudo man-db man-pages texinfo networkmanager curl git firefox-developer-edition grub efibootmgr amd-ucode dkms linux-headers xorg xorg-server gdm i3-gaps i3blocks i3status i3lock dmenu alacritty
