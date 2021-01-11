91cf901fa5d9795572540e6c551cc510e39b54d6
Simon-A-P <simon.pfaus@web.de>
2021-01-11 11:40:52 +0100

    neovim nightly->latest, modprobe btusb?

--- arch-configure.sh
+++ arch-configure.sh
@@ -6,9 +6,9 @@ cd $HOME/dotfiles
 sudo pacman -Syyu --noconfirm
 yay -Syyu --noconfirm
 
-sudo pacman -S --noconfirm base base-devel linux linux-firmware reflector sudo man-db man-pages texinfo networkmanager curl git firefox-developer-edition grub efibootmgr amd-ucode dkms linux-headers xorg xorg-server gnome gnome-tweaks rustup noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra cups bluez bluez-utils
+sudo pacman -S --noconfirm base base-devel linux linux-firmware reflector sudo man-db man-pages texinfo networkmanager curl git neovim firefox-developer-edition grub efibootmgr amd-ucode dkms linux-headers xorg xorg-server gnome gnome-tweaks rustup noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra cups bluez bluez-utils
 
-yay -S --noconfirm gnome-shell-extension-pop-shell yay neovim-nightly visual-studio-code-bin
+yay -S --noconfirm gnome-shell-extension-pop-shell yay visual-studio-code-bin
 
 # Load all dconf settings
 dconf load / < ./dconf/full-backup
@@ -21,6 +21,8 @@ sudo chown -R root:root ./root
 for file in $(find $HOME/dotfiles/root -type f); do sudo mkdir -p $(dirname $(echo $file | sed -r "s/\/home\/$USER\/dotfiles\/root//")) && sudo ln -sf $file $(echo $file | sed -r "s/\/home\/$USER\/dotfiles\/root//"); done
 
 rustup default stable
+
+# sudo modprobe btusb
 
 sudo systemctl enable NetworkManager.service
 sudo systemctl enable gdm.service
