834b3dec45eeb09dcb140afd09326e9cfbdc642e
Simon-A-P <simon.pfaus@web.de>
2021-01-20 13:46:13 +0100

    Move programs from device-specfic to general configuration

--- spfaus-honor/arch-configure.sh
+++ spfaus-honor/arch-configure.sh
@@ -2,6 +2,4 @@
 # This script handles device-specific configuration and is called by the general configuration script.
 set -ex
 
-sudo pacman -S --noconfirm filezilla thunderbird chromium
-
 ln -sf $HOME/dotfiles/spfaus-honor/.gitconfig $HOME/.gitconfig
