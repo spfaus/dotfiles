10a1955fdf1e71f0b9fe42b39491fb42d11b6448
Simon-A-P <simon.pfaus@web.de>
2021-06-20 17:58:28 +0200

    Blacklist faulty kernel drivers for rtl8821ce on linux kernel 5.12

--- config-groups/rtl8821ce/configure.sh
+++ config-groups/rtl8821ce/configure.sh
@@ -2,3 +2,7 @@
 set -ex
 
 yay -Sy --noconfirm rtl8821ce-dkms-git
+
+# Link all root config files and change owner to root
+sudo chown -R root:root $HOME/dotfiles/config-groups/rtl8821ce/root/
+sudo cp -as --remove-destination $HOME/dotfiles/config-groups/rtl8821ce/root/. /
