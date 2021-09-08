a5fd58895ccac56ac62d9f93fec960e731b8543a
Simon-A-P <simon.pfaus@web.de>
2021-09-08 18:19:37 +0200

    Add vpnc for work VPN

--- config-groups/work/configure.sh
+++ config-groups/work/configure.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-yay -S --noconfirm phpstorm-jre phpstorm filezilla thunderbird slack-desktop docker docker-compose docker-machine chromium
+yay -S --noconfirm phpstorm-jre phpstorm filezilla thunderbird slack-desktop docker docker-compose docker-machine chromium vpnc networkmanager-vpnc
 
 sudo systemctl enable docker.service
 sudo gpasswd -a $USER docker
