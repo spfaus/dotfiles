ea38c6e695b55d5f9637f89f93d2c925ee12e39b
Simon-A-P <simon.pfaus@web.de>
2021-09-10 00:20:38 +0200

    Remove repo version check but leave user choice whether to use local or remote version

--- configure.sh
+++ configure.sh
@@ -4,36 +4,13 @@ set -ex
 cd $HOME/dotfiles
 
 # Check if local dotfile repo is up to date
-status=-1
-if [ "`git log --pretty=%H ...refs/heads/master^ | head -n 1`" = "`git ls-remote origin -h refs/heads/master |cut -f1`" ]; then
-    status=0
-    statustxt="Local repository is up to date."
-else
-    status=2
-    statustxt="New changes on remote."
-fi
-
-if [[ `git status --porcelain` ]]; then
-    status=1
-    statustxt="Local repository has uncommitted changes."
-fi
-
-if [ $stats -eq -1 ]; then
-    echo "Error: Could not verify repo status";
-    exit $ERRCODE;
-fi
-
-if [ $status -ne 0 ]; then
-    echo $statustxt
-    echo "Do you wish to discard local changes and pull from remote?"
-    select yn in "Yes" "No"; do
-        case $yn in
-            Yes ) git reset HEAD --hard && git pull; break;;
-            No ) break;;
-        esac
-    done
-fi
-
+echo "Do you wish to discard local, checkout master and pull remote dotfile changes before running configuration?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) git reset HEAD --hard && checkout master && git reset HEAD --hard && git pull; break;;
+        No ) break;;
+    esac
+done
 
 # Start configuration script based on device
 read -p "Enter device name (hp14s, spfaus-honor, simon-desktop): " DEVICE_NAME
