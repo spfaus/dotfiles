ad29eca285877477fcbb165b5c69c6f9499e132a
Simon-A-P <simon.pfaus@web.de>
2021-09-10 01:05:08 +0200

    Make reboot a y/n choice

--- configure.sh
+++ configure.sh
@@ -4,7 +4,7 @@ set -e
 cd $HOME/dotfiles
 
 # Use local or remote state for configuration
-echo "Do you wish to discard local, checkout master and pull remote dotfile changes before running configuration?"
+echo "Do you wish to discard local changes, checkout master, and pull from remote before running configuration?"
 select yn in "Yes" "No"; do
     case $yn in
         Yes ) git reset HEAD --hard && git checkout master && git reset HEAD --hard && git pull; break;;
@@ -17,5 +17,10 @@ read -p "Enter device name (hp14s, spfaus-honor, simon-desktop): " DEVICE_NAME
 type $HOME/dotfiles/devices/$DEVICE_NAME/configure.sh && $HOME/dotfiles/devices/$DEVICE_NAME/configure.sh
 
 # Optional reboot
-read -p "Press enter to reboot"
-reboot
+echo "Reboot now?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) reboot; break;;
+        No ) break;;
+    esac
+done
