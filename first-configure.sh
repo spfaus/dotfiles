3298f505b5a3defbd267adabe63d7b14c3a47314
Simon Alexander Pfaus <simon.pfaus@web.de>
2021-09-30 06:29:29 -0400

    Update first-configure.sh

--- first-configure.sh
+++ first-configure.sh
@@ -4,7 +4,7 @@ set -e
 cd $HOME/dotfiles
 
 sudo pacman -Sy --noconfirm git reflector rsync
-sudo reflector --verbose --country Germany --latest 50 --sort rate --save /etc/pacman.d/mirrorlist
+sudo reflector --verbose --country Canada --latest 20 --sort rate --save /etc/pacman.d/mirrorlist
 
 git clone https://aur.archlinux.org/yay.git
 cd yay
