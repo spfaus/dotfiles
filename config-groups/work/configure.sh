6b1a35db9305f170b8b0c4ab028fbc819b677115
Simon Alexander Pfaus <simon.pfaus@web.de>
2021-09-30 06:10:10 -0400

    Add zoom to work config

--- config-groups/work/configure.sh
+++ config-groups/work/configure.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-yay -S --noconfirm phpstorm-jre phpstorm slack-desktop firefox-developer-edition chromium microsoft-edge-dev-bin vpnc networkmanager-vpnc docker docker-compose docker-machine
+yay -S --noconfirm phpstorm-jre phpstorm slack-desktop firefox-developer-edition chromium microsoft-edge-dev-bin vpnc networkmanager-vpnc docker docker-compose docker-machine zoom
 
 sudo systemctl enable docker.service
 sudo gpasswd -a $USER docker
