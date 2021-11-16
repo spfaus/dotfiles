b07c11622f0ad9c652923ce4c12747b24a75a8b5
Simon-A-P <simon.pfaus@web.de>
2021-11-16 13:12:28 -0500

    Remove rtl8821ce config group since there now is a functional driver in the kernel

--- config-groups/rtl8821ce/configure.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-yay -S --noconfirm rtl8821ce-dkms-git
-
-# Link all root config files and change owner to root
-sudo chown -R root:root $HOME/dotfiles/config-groups/rtl8821ce/root/
-sudo cp -as --remove-destination $HOME/dotfiles/config-groups/rtl8821ce/root/. /
