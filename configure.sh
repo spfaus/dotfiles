c1d8bbca7be34151ab9022f3cd500a17de858e9b
Simon-A-P <simon.pfaus@web.de>
2022-01-16 23:44:50 -0500

    Add cups

--- configure.sh
+++ configure.sh
@@ -46,7 +46,7 @@ rustup default stable
 rustup update
 cargo install cargo-generate cargo-watch cargo-edit
 
-yay -S --noconfirm base base-devel linux linux-firmware reflector sudo man-db man-pages texinfo networkmanager curl wget rsync git grub efibootmgr dkms linux-headers xorg xorg-server gnome gnome-tweaks noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra gnome-shell-extension-pop-shell-git yay neovim ntfs-3g chromium vundle-git nodejs yarn amd-ucode discord fish alacritty ripgrep
+yay -S --noconfirm base base-devel linux linux-firmware reflector sudo man-db man-pages texinfo networkmanager curl wget rsync git grub efibootmgr dkms linux-headers xorg xorg-server gnome gnome-tweaks noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra gnome-shell-extension-pop-shell-git yay neovim ntfs-3g chromium vundle-git nodejs yarn amd-ucode discord fish alacritty ripgrep cups
 
 # Load all dconf settings                                                                                                                                                          
 dconf load / < $HOME/dotfiles/dconf/full-backup
@@ -60,6 +60,7 @@ sudo cp -as --remove-destination $HOME/dotfiles/root/. /
 
 sudo systemctl enable NetworkManager.service
 sudo systemctl enable gdm.service
+sudo systemctl enable cups.service
 
 sudo ln -sf /usr/bin/nvim /usr/bin/vim
 sudo ln -sf /usr/bin/nvim /usr/bin/vi
