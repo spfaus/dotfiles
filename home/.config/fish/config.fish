0129c61bf3e6a6a31d2dfaea401de959b2989518
spfaus <simon.pfaus@web.de>
2023-02-03 09:36:37 -0500

    Add cargo binaries to PATH

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -2,6 +2,7 @@ if status is-interactive
     alias ls="ls -lAh --color"
     alias :q="exit"
     alias :q!="exit"
+    export PATH="$HOME/.cargo/bin:$PATH"
     function fish_prompt
         powerline-shell --shell bare $status
     end
