34a6fe7593b1e4280db7ec23af27f4705f266a52
spfaus <simon.pfaus@web.de>
2023-02-07 14:31:30 -0500

    Esc to exit terminal mode - nvim

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -146,6 +146,9 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
+-- Remap for exiting terminal-mode more easily
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { desc = '[<Esc>] to escape terminal mode' })
+
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
