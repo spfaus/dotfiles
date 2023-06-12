62c578d75d1ceaea2e05a3764ca52b385f80692e
spfaus <simon.pfaus@web.de>
2023-06-12 11:15:38 -0400

    Remove multiple language servers; Update helix config;

--- configure.sh
+++ configure.sh
@@ -59,9 +59,7 @@ yay -S --noconfirm base base-devel linux linux-firmware reflector sudo man-db ma
     ddev-bin mkcert dialog jq \
     libreoffice-fresh \
     php composer \
-    helix lldb xclip xsel wl-clipboard vscode-html-languageserver nodejs-intelephense \
-    vscode-css-languageserver typescript-language-server bash-language-server \
-    dockerfile-language-server vscode-json-languageserver marksman yaml-language-server
+    helix lldb xclip xsel wl-clipboard
 
 # Install Visual Studio Code extensions
 code \
