fe07ea461c9ec580484504e9c33073c6c5abbfa7
spfaus <simon.pfaus@web.de>
2023-07-26 09:16:04 -0400

    Switch to JetBrainsMono Nerd Font

--- configure.sh
+++ configure.sh
@@ -49,7 +49,7 @@ rustup component add rust-analyzer
 yay -S --noconfirm base base-devel linux linux-firmware reflector sudo man-db man-pages \
     texinfo networkmanager curl wget rsync git grub efibootmgr dkms linux-headers \
     xorg-server gnome-tweaks noto-fonts noto-fonts-cjk noto-fonts-emoji \
-    noto-fonts-extra gnome-shell-extension-pop-shell-git yay ntfs-3g chromium \
+    noto-fonts-extra ttf-jetbrains-mono-nerd gnome-shell-extension-pop-shell-git yay ntfs-3g chromium \
     firefox amd-ucode discord fish cups bitwarden anki neovim \
     powerline-shell autojump-rs htop visual-studio-code-bin \
     lua-language-server grub-customizer \
