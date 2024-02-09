34079e6bd91d4ca3ed58156de10a065f0516f5b9
spfaus <simon.pfaus@web.de>
2024-02-08 19:24:57 -0500

    replace init.vim with basic init.lua

---
+++ home/.config/nvim/init.lua
@@ -0,0 +1,7 @@
+vim.cmd [[colorscheme slate]]
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.smartindent = true
+vim.opt.shiftwidth = 4
+vim.opt.colorcolumn = '80'
+vim.api.nvim_set_hl(0, "ColorColumn", { ctermbg=238, bg=LightGrey })
