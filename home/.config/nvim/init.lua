1b51556dddac911f98e008b57435e21160650d6f
spfaus <simon.pfaus@web.de>
2024-02-10 02:39:26 -0500

    add neovim rust lsp

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -5,3 +5,17 @@ vim.opt.smartindent = true
 vim.opt.shiftwidth = 4
 vim.opt.colorcolumn = '80'
 vim.api.nvim_set_hl(0, "ColorColumn", { ctermbg=238, bg=LightGrey })
+
+local autocmd = vim.api.nvim_create_autocmd
+autocmd("FileType", {
+    pattern = "rust",
+    callback = function()
+	local root_dir = vim.fs.dirname(vim.fs.find({'Cargo.toml'}, { upward = true })[1])
+	local client = vim.lsp.start({
+	    name = 'rust-analyzer',
+	    cmd = {'rust-analyzer'},
+	    root_dir = root_dir,
+	})
+	vim.lsp.buf_attach_client(0, client)
+    end
+})
