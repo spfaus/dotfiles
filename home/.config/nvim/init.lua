b9d11018139ba7008ff3f8c5c8b9f2332c3000b6
spfaus <simon.pfaus@web.de>
2024-02-12 15:25:50 -0500

    nvim no softwrap

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -4,6 +4,7 @@ vim.opt.relativenumber = true
 vim.opt.smartindent = true
 vim.opt.shiftwidth = 4
 vim.opt.colorcolumn = '80'
+vim.opt.wrap = false
 vim.api.nvim_set_hl(0, "ColorColumn", { ctermbg = 238, bg = LightGrey })
 
 local autocmd = vim.api.nvim_create_autocmd
