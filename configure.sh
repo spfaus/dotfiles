37332698d16545b67a61dd60538f35e5365d9b3d
spfaus <simon.pfaus@web.de>
2024-07-06 15:05:44 -0400

    Add bat

--- configure.sh
+++ configure.sh
@@ -63,7 +63,8 @@ yay -S --noconfirm base base-devel linux linux-firmware reflector sudo man-db ma
     dbeaver \
     libfido2 \
     authenticator \
-    ncdu
+    ncdu \
+    bat
 
 # Load all dconf settings
 dconf load / < $(pwd)/dconf/full-backup
