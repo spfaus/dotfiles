619cbc2473e19ef51651f469f5f329cf29dd7d12
spfaus <simon.pfaus@web.de>
2024-07-13 08:16:18 -0400

    set softtabstop and expandtab

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -14,6 +14,8 @@ vim.opt.relativenumber = true
 vim.opt.smartindent = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
 vim.opt.wrap = false
 vim.opt.scrolloff = 8
 vim.opt.ignorecase = true
@@ -28,41 +30,41 @@ vim.opt.cursorcolumn = true
 local autocmd = vim.api.nvim_create_autocmd
 
 autocmd("FileType", {
-	pattern = "rust",
-	callback = function()
-		local root_dir = vim.fs.dirname(vim.fs.find({ 'Cargo.toml' }, { upward = true })[1])
-		local client = vim.lsp.start({
-			name = 'rust-analyzer',
-			cmd = { 'rust-analyzer' },
-			root_dir = root_dir,
-		})
-		vim.lsp.buf_attach_client(0, client)
-	end
+    pattern = "rust",
+    callback = function()
+        local root_dir = vim.fs.dirname(vim.fs.find({ 'Cargo.toml' }, { upward = true })[1])
+        local client = vim.lsp.start({
+            name = 'rust-analyzer',
+            cmd = { 'rust-analyzer' },
+            root_dir = root_dir,
+        })
+        vim.lsp.buf_attach_client(0, client)
+    end
 })
 
 autocmd("FileType", {
-	pattern = "lua",
-	callback = function()
-		local root_dir = vim.fs.dirname(vim.fs.find({ 'init.lua', 'init.vim', '.git' }, { upward = true })[1])
-		local client = vim.lsp.start({
-			name = 'lua-language-server',
-			cmd = { 'lua-language-server' },
-			root_dir = root_dir,
-			settings = { Lua = { diagnostics = { globals = { 'vim', 'LightGrey' } } } },
-		})
-		vim.lsp.buf_attach_client(0, client)
-	end
+    pattern = "lua",
+    callback = function()
+        local root_dir = vim.fs.dirname(vim.fs.find({ 'init.lua', 'init.vim', '.git' }, { upward = true })[1])
+        local client = vim.lsp.start({
+            name = 'lua-language-server',
+            cmd = { 'lua-language-server' },
+            root_dir = root_dir,
+            settings = { Lua = { diagnostics = { globals = { 'vim', 'LightGrey' } } } },
+        })
+        vim.lsp.buf_attach_client(0, client)
+    end
 })
 
 autocmd('LspAttach', {
-	callback = function(args)
-		local client = vim.lsp.get_client_by_id(args.data.client_id)
-		if client.server_capabilities.hoverProvider then
-			vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = args.buf })
-		end
-	end,
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client.server_capabilities.hoverProvider then
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = args.buf })
+        end
+    end,
 })
 
 autocmd("BufWritePre", {
-	command = "lua vim.lsp.buf.format()"
+    command = "lua vim.lsp.buf.format()"
 })
