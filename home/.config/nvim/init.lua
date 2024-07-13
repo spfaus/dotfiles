d985b0cc214f66ab4eff963633360a31e111e767
spfaus <simon.pfaus@web.de>
2024-07-13 10:21:02 -0400

    Remove lsp and some settings from nvim

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -4,6 +4,7 @@
 -- switch ; and :
 -- leader y/p to use system clipboard
 -- when reopening a file, jump to last edited location or location where closed
+-- column 80 highlight
 
 vim.g.mapleader = " "
 vim.cmd('colorscheme slate')
@@ -21,50 +22,47 @@ vim.opt.scrolloff = 8
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.undofile = true
---vim.opt.clipboard = unnamedplus -- Needs to be fixed (provider not working?)
 vim.opt.cursorline = true
 vim.opt.cursorcolumn = true
---vim.opt.colorcolumn = '80'
---vim.api.nvim_set_hl(0, "ColorColumn", { ctermbg = 238, bg = LightGrey }) -- does not work with termguicolors
 
-local autocmd = vim.api.nvim_create_autocmd
-
-autocmd("FileType", {
-    pattern = "rust",
-    callback = function()
-        local root_dir = vim.fs.dirname(vim.fs.find({ 'Cargo.toml' }, { upward = true })[1])
-        local client = vim.lsp.start({
-            name = 'rust-analyzer',
-            cmd = { 'rust-analyzer' },
-            root_dir = root_dir,
-        })
-        vim.lsp.buf_attach_client(0, client)
-    end
-})
-
-autocmd("FileType", {
-    pattern = "lua",
-    callback = function()
-        local root_dir = vim.fs.dirname(vim.fs.find({ 'init.lua', 'init.vim', '.git' }, { upward = true })[1])
-        local client = vim.lsp.start({
-            name = 'lua-language-server',
-            cmd = { 'lua-language-server' },
-            root_dir = root_dir,
-            settings = { Lua = { diagnostics = { globals = { 'vim', 'LightGrey' } } } },
-        })
-        vim.lsp.buf_attach_client(0, client)
-    end
-})
-
-autocmd('LspAttach', {
-    callback = function(args)
-        local client = vim.lsp.get_client_by_id(args.data.client_id)
-        if client.server_capabilities.hoverProvider then
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = args.buf })
-        end
-    end,
-})
-
-autocmd("BufWritePre", {
-    command = "lua vim.lsp.buf.format()"
-})
+--local autocmd = vim.api.nvim_create_autocmd
+--
+--autocmd("FileType", {
+--    pattern = "rust",
+--    callback = function()
+--        local root_dir = vim.fs.dirname(vim.fs.find({ 'Cargo.toml' }, { upward = true })[1])
+--        local client = vim.lsp.start({
+--            name = 'rust-analyzer',
+--            cmd = { 'rust-analyzer' },
+--            root_dir = root_dir,
+--        })
+--        vim.lsp.buf_attach_client(0, client)
+--    end
+--})
+--
+--autocmd("FileType", {
+--    pattern = "lua",
+--    callback = function()
+--        local root_dir = vim.fs.dirname(vim.fs.find({ 'init.lua', 'init.vim', '.git' }, { upward = true })[1])
+--        local client = vim.lsp.start({
+--            name = 'lua-language-server',
+--            cmd = { 'lua-language-server' },
+--            root_dir = root_dir,
+--            settings = { Lua = { diagnostics = { globals = { 'vim', 'LightGrey' } } } },
+--        })
+--        vim.lsp.buf_attach_client(0, client)
+--    end
+--})
+--
+--autocmd('LspAttach', {
+--    callback = function(args)
+--        local client = vim.lsp.get_client_by_id(args.data.client_id)
+--        if client.server_capabilities.hoverProvider then
+--            vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = args.buf })
+--        end
+--    end,
+--})
+--
+--autocmd("BufWritePre", {
+--    command = "lua vim.lsp.buf.format()"
+--})
