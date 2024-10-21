e9c670cfb370737709a228660aa2acc69a32c2a6
spfaus <simon.pfaus@web.de>
2024-10-21 02:12:41 -0400

    Neovim configuration

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -1,5 +1,4 @@
 -- TODOS
--- colorscheme
 -- php integration / lsp
 
 -- Bootstrap lazy.nvim
@@ -21,7 +20,7 @@ vim.opt.rtp:prepend(lazypath)
 
 -- Make sure to setup `mapleader` and `maplocalleader`
 vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+vim.g.maplocalleader = " "
 
 -- Setup lazy.nvim
 require("lazy").setup({
@@ -50,18 +49,39 @@ vim.opt.cursorline = true
 --vim.opt.cursorcolumn = true
 vim.opt.colorcolumn = "100"
 
+-- Functionality
 --vim.api.nvim_set_hl(0, "ColorColumn", { ctermbg=233, bg="#222222" })
 vim.cmd('filetype plugin on')
+
 vim.opt.number = true
 vim.opt.relativenumber = true
+
 vim.opt.smartindent = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
 vim.opt.wrap = false
+vim.opt.breakindent = true
+
 vim.opt.scrolloff = 8
+
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
+
 vim.opt.undofile = true
 
+vim.opt.mouse = 'a'
+
+vim.opt.signcolumn = 'yes'
+
+vim.opt.updatetime = 250
+--vim.opt.timeoutlen = 300
+
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+vim.opt.inccommand = 'split'
