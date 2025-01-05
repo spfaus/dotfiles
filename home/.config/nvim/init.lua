1d40c0da8d9aaa819d6759e5088b223d3744fc6c
spfaus <simon.pfaus@web.de>
2025-01-04 21:43:26 -0500

    Remove nvim-treesitter

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -1,6 +1,3 @@
--- TODOS
--- php integration / lsp
-
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -27,35 +24,10 @@ require("lazy").setup({
     spec = {
         { "echasnovski/mini.pairs", version = '*', opts = {} },
         { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = {} },
-        { "nvim-treesitter/nvim-treesitter", version = '*', opts = {}, build = ":TSUpdate" },
     },
     install = { colorscheme = { "gruvbox" } },
     checker = { enabled = false },
 })
-
-require'nvim-treesitter.configs'.setup {
-    ensure_installed = {},
-    sync_install = false,
-    auto_install = true,
-    highlight = {
-        enable = true,
-        -- disable slow treesitter highlight for large files
-        disable = function(lang, buf)
-            local max_filesize = 100 * 1024 -- 100 KB
-            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-            if ok and stats and stats.size > max_filesize then
-                return true
-            end
-        end,
-
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        additional_vim_regex_highlighting = false,
-    },
-    indent = {
-        enable = true
-    }
-}
 
 -- Keybindings
 vim.keymap.set('n', '<Leader>y', '"+y')
@@ -66,8 +38,6 @@ vim.keymap.set('v', '<Leader>y', '"+y')
 vim.keymap.set('v', '<Leader>Y', '"+Y')
 vim.keymap.set('v', '<Leader>p', '"+p')
 vim.keymap.set('v', '<Leader>P', '"+P')
-
-vim.cmd('filetype plugin on')
 
 -- Colors
 vim.cmd('colorscheme gruvbox')
